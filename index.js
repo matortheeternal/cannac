1c65e630dd961b6dec25f543913b57a7464a664d
Mator <why101@msn.com>
2021-11-22 18:40:59 -0800

    index

---
+++ index.js
@@ -0,0 +1,8 @@
+const Ability = require('./src/ability');
+const {addResourceAlias, addActionAlias} = require('./src/aliasService');
+
+module.exports = {
+  Ability,
+  addResourceAlias,
+  addActionAlias
+};
